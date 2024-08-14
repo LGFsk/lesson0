a65bbb6f94f9dd0792ee1034137b72f9e555001a
Евгений Коротков <77969406+LGFsk@users.noreply.github.com>
2024-08-14 22:17:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+
+print(5/5)
+print(5//5)
+print(type(5//3))
+print(type(5/3))
+print(5/3)
+print(5%3)
+print(5**3)
+print(2.00)
+print(type("rfdsf"))
+print("Света ' пру та та") # // двойная кавычка /
+print('Света " пру та та') # одинарная кавычка
+print("1+2'"'"'"'2'"'"1"') # набор ковычек в результате
+print("2+2.0"+"F")
+print(True,False)
+print(type(True),type(False))
+print(5>10)
+a=5
+b=4
+print(a)
+print(a != b and b>b)
+print(a != b and a>b)
+print(a != b and a>b)
+print(a != b or b>b)
+print(int('5'))
+print(type(int(3)))
+print(type(str(3+3)))
+print((str(3+3)))
+print(((3+3)))
+
+print(12 + 89 - 1)
+
+# Задание
+print('Задача 1 (просто) "Арифметика":')
+print("1) ""1st program""'")
+print("1st program""'.")
+print("2) ",9**0.5*5)
+print('Задача 2 (просто) "Логика":')
+print('"2nd program'"'")
+print(9.99>9.98 and 1000!=1000.1)
+print('Задача 3 (средне) "Школьная загадка":')
+print('"3rd program'"'")
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+print('Задача 4 (сложно) "Первый после точки":')
+print('"4th program'"'")
+print(int((float('123.456')*10)%1230))
+print(int((float('123.456')*10)-1230.56))
