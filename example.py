83a7daa95b52a0ecc7bdb8a879bae847b0ca57cb
Евгений Коротков <77969406+LGFsk@users.noreply.github.com>
2024-08-15 17:50:27 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,5 @@ example = 'Urban'
 print(example[0])
 print(example[-1])
 print(example[2:])
-# или
-print(example[len(example)//2:])
 print(example[::-1])
-print(example[1:10:2])
+print(example[1:4:2])
